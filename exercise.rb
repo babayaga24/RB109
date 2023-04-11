699456e7847a5ea51a766ab5ba2db13186a78601
babayaga24 <kumarabhishek2407@gmail.com>
2023-04-11 16:02:31 +0530

    Practice exercises for RB109

---
+++ exercise.rb
@@ -0,0 +1,49 @@
+# def example(str)
+#   i = 3
+#   loop do 
+#     puts str 
+#     i -= 1 
+#     break if i == 0 
+#   end
+# end
+
+# example('hello')
+
+# def greetings(str)
+#   puts str
+#   puts "Goodbye"
+# end
+
+# word = "Hello"
+# greetings(word)
+
+# arr = [1, 2, 3, 4]
+# counter = 0
+# sum = 0
+
+# loop do
+#   sum += arr[counter]
+#   counter += 1
+#   break if counter == arr.size
+# end 
+
+# puts "Your total is #{sum}"
+
+# a = 'Bob'
+
+# 5.times do |x|
+#   a = 'Bill'
+# end
+
+# p a
+
+animal = "dog"
+
+loop do |_|
+  animal = "cat"
+  var = "ball"
+  break
+end
+
+puts animal
+puts var
