1937a16a9586f6c6269d0d45ff80c08108d064bc
babayaga24 <kumarabhishek2407@gmail.com>
2023-04-11 16:10:31 +0530

    Variable Shadowing Examples

---
+++ variable_shadowing.rb
@@ -0,0 +1,25 @@
+# a = 4
+# b = 2
+
+# 2.times do |a|
+#   a = 5
+#   puts a
+# end
+
+# puts a
+# puts b
+
+# n = 10
+# 1.times do |n|
+#   n = 11
+# end
+
+# puts n
+
+animal = "dog"
+loop do |animal|
+  animal = "cat" 
+  break
+end
+
+puts animal
